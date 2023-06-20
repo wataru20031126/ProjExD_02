c50b2736a1efbeca6b4d6afa6f753cde8f09b3fd
wataru <c0a220649f@edu.teu.ac.jp>
2023-06-20 14:14:28 +0900

    練習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,7 @@
+import random
 import sys
 import pygame as pg
+
 
 
 WIDTH, HEIGHT = 1600, 900
@@ -13,6 +15,14 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
     tmr = 0
+    #練習1
+    bb_img = pg.Surface((20, 20))  # ボムのサーフェイスを作成
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # ボムを描画
+    bb_img.set_colorkey((0, 0, 0)) #背景を透明
+    x = random.randint(0,WIDTH)
+    y = random.randint(0,HEIGHT) 
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = x,y #爆弾rectの中心座標を乱数で指定する。
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -20,6 +30,9 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bb_img,bb_rct)
+
+    
         pg.display.update()
         tmr += 1
         clock.tick(10)
